507fc051a73b71d8443c7e4871008ad0e6e558e5
Zack Bumpous <upwork.ztb@gmail.com>
2019-12-17 12:13:30 -0600

    for TL

--- api/server.js
+++ api/server.js
@@ -1,21 +1,30 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
-
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const sessions = require("express-session"); // <<<<< install express-session
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 const server = express();
-
+const sessionConfiguration = {
+  // session storage options
+  name: "chocolatechip", // default would be sid
+  secret: "keep it secret, keep it safe!", // used for encryption (must be an environment variable)
+  saveUninitialized: true, // has implications with GDPR laws
+  resave: false,
+  // cookie options
+  cookie: {
+    maxAge: 1000 * 60 * 10, // 10 mins in milliseconds
+    secure: false, // if false the cookie es sent over http, if true only sent over https
+    httpOnly: true, // if true JS cannot access the cookie
+  },
+};
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
-
-server.get('/', (req, res) => {
-  res.json({ api: 'up' });
+server.use(sessions(sessionConfiguration)); // add a req.session object
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
 });
-
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -18,18 +18,19 @@ router.post('/register', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   let { username, password } = req.body;
-
   Users.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        // save a session for the client and send back a cookie
+        req.session.user = user;
         res.status(200).json({
           message: `Welcome ${user.username}!`,
         });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {
